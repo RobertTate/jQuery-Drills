399b0a5ff050bea9fdc64037c958b6d5147e5307
Robert Tate <robertjosephtate@gmail.com>
2018-06-18 14:44:09 -0500

    removed comments

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ $(document).ready(function () {
 
     $('#button-id').attr('disabled', true);
 
-
-    // $('#button-id').click(function () {
-    //     content = $("#input-field").val();
-    //     alert(content);
-    // });
-
-
     $('#input-field').on('input', function () {
         let inputField = $('#input-field').val();
         if (inputField.length >= 1) {
@@ -20,6 +13,7 @@ $(document).ready(function () {
     });
 
     $('body').append('<div id="div-id"></div>');
+   
     $('#div-id').append(`<ul id="ul-id"</ul>`);
 
     $('#button-id').click(function (event) {
@@ -27,13 +21,6 @@ $(document).ready(function () {
         target = $('#h2-id');
         content = $("#input-field").val();
         $('#ul-id').append(`<li id="li-id">${content}</li>`);
-
-        // $('li').mouseover(function (event) {
-        //     $(event.currentTarget).css({
-        //         'background-color': 'red',
-        //         'border-radius': '3rem'
-        //     });
-        // });
 
         $('li').click(function (event) {
             $(event.currentTarget).css({
@@ -51,8 +38,5 @@ $(document).ready(function () {
         let randColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
         return randColor;
     }
-
-
-
 
 });
